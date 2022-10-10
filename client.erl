b35ef67ce9900e5170e5e5d4362fe5f45525f2d5
Gx3 Studios <gabriel.guardiola@estudiantat.upc.edu>
2022-10-10 11:59:41 +0200

    v1.1

--- client.erl
+++ client.erl
@@ -8,7 +8,7 @@ start(ServerPid, MyName) ->
     process_commands(ServerPid, MyName, ClientPid).
 
 init_client(ServerPid, MyName) ->
-    ServerPid ! {client_join_req, ..., ...},  %% TODO: COMPLETE
+    ServerPid ! {client_join_req, MyName, self()},  %% TODO: COMPLETE
     process_requests().
 
 %% Local Functions
@@ -17,13 +17,14 @@ process_requests() ->
     receive
         {join, Name} ->
             io:format("[JOIN] ~s joined the chat~n", [Name]),
-            %% TODO: ADD SOME CODE
+            process_requests();
+            
         {leave, Name} ->
-            %% TODO: ADD SOME CODE
+            io:format("[EXIT] ~s left~n", [Name]),
             process_requests();
         {message, Name, Text} ->
             io:format("[~s] ~s", [Name, Text]),
-            %% TODO: ADD SOME CODE
+            process_requests();
         exit -> 
             ok
     end.
@@ -34,9 +35,9 @@ process_commands(ServerPid, MyName, ClientPid) ->
     Text = io:get_line("-> "),
     if 
         Text  == "exit\n" ->
-            ServerPid ! {client_leave_req, ..., ...},  %% TODO: COMPLETE
+            ServerPid ! {client_leave_req, MyName, ClientPid},  %% TODO: COMPLETE
             ok;
         true ->
-            ServerPid ! {send, ..., ...},  %% TODO: COMPLETE
+            ServerPid ! {send, MyName, Text},  %% TODO: COMPLETE
             process_commands(ServerPid, MyName, ClientPid)
     end.
